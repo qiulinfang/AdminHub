976e5926ea47bfe0d71ef6fe3c8e79aa16836bbb
fangql <2054963713@qq.com>
2024-06-07 20:32:44 +0800

    配置开发环境

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url';
+const resolve = (p) => fileURLToPath(new URL(p, import.meta.url));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+    },
+  },
+})
